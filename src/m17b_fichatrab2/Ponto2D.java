39d4f04c438562d0562164278986bc18223b8d48
a13287 <a13287@d1-08-11.dped.local>
2017-02-07 16:23:40 +0000

    verção beta 0.1

--- src/m17b_fichatrab2/Ponto2D.java
+++ src/m17b_fichatrab2/Ponto2D.java
@@ -3,17 +3,37 @@ package m17b_fichatrab2;
 public class Ponto2D {
     private int x;
     private int y;
-    
-    // Faltam construtor, getters e setters.
-    
+
+    public Ponto2D(){
+        x = 0;
+        y = 0;        
+    }
+    /**
+     * 
+     * @param x
+     * @param y 
+     */
     public Ponto2D(int x, int y){
         this.x = x;
         this.y = y;        
     }
-    
+    /**
+     * 
+     * @param a
+     * @return 
+     */
     public double distancia(Ponto2D a){
-        // Falta fazer este método.
-        return 0.0;
+        double dx = x - a.x;
+        double dy = y - a.y;
+        return Math.sqrt(dx*dx + dy*dy);
     }
-    
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public String toString(){
+        return("X="+x+" Y="+y);
+    }
 }
