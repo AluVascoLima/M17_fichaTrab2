4651d76849365224e8a1930afbcd3a4d14804906
a13287 <a13287@d1-08-11.dped.local>
2017-02-08 10:45:01 +0000

    verção beta 0.2

--- src/m17b_fichatrab2/Ponto2D.java
+++ src/m17b_fichatrab2/Ponto2D.java
@@ -1,31 +1,61 @@
 package m17b_fichatrab2;
 
 public class Ponto2D {
+
     private int x;
     private int y;
 
-    public Ponto2D(){
+    public Ponto2D() {
         x = 0;
-        y = 0;        
+        y = 0;
     }
+
     /**
-     * 
+     *
      * @param x
-     * @param y 
+     * @param y
      */
-    public Ponto2D(int x, int y){
+    public Ponto2D(int x, int y) {
         this.x = x;
-        this.y = y;        
+        this.y = y;
     }
     /**
      * 
-     * @param a
      * @return 
      */
-    public double distancia(Ponto2D a){
+    public int getX(){
+        return x;
+    }
+    /**
+     * 
+     * @return 
+     */
+    public int gety(){
+        return x;
+    }
+    /**
+     * 
+     * @param x 
+     */
+    public void setX(int x){
+        this.x = x;
+    }
+    /**
+     * 
+     * @param y 
+     */
+    public void setY(int y){
+        this.y = y;
+    }
+    /**
+     *
+     * @param a
+     * @return
+     */
+    public double distancia(Ponto2D a) {
         double dx = x - a.x;
         double dy = y - a.y;
-        return Math.sqrt(dx*dx + dy*dy);
+        return Math.sqrt((dx * dx) + (dy * dy));
     }
 
     /**
@@ -33,7 +63,7 @@ public class Ponto2D {
      * @return
      */
     @Override
-    public String toString(){
-        return("X="+x+" Y="+y);
+    public String toString() {
+        return ("X=" + x + " Y=" + y);
     }
 }
