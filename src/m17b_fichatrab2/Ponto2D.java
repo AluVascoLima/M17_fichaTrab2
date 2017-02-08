20686ecbd88a4821f5e0de27561476265cd004ca
a13287 <a13287@d1-08-11.dped.local>
2017-02-08 11:33:24 +0000

    verção beta 0.4

--- src/m17b_fichatrab2/Ponto2D.java
+++ src/m17b_fichatrab2/Ponto2D.java
@@ -4,53 +4,48 @@ public class Ponto2D {
 
     private int x;
     private int y;
-
+    
+    /**
+     * Inicializa as coordenadas sem parametros
+     */
     public Ponto2D() {
         x = 0;
         y = 0;
     }
 
     /**
-     *
-     * @param x
-     * @param y
+     * Inicializa as coordenadas com parametros
      */
     public Ponto2D(int x, int y) {
         this.x = x;
         this.y = y;
     }
     /**
-     * 
-     * @return 
+     * @return coordenada x
      */
     public int getX(){
         return x;
     }
     /**
-     * 
-     * @return 
+     * @return coordenada y
      */
     public int gety(){
         return x;
     }
     /**
-     * 
-     * @param x 
+     * Setter para x
      */
     public void setX(int x){
         this.x = x;
     }
     /**
-     * 
-     * @param y 
+     * Setter para y
      */
     public void setY(int y){
         this.y = y;
     }
     /**
-     *
-     * @param a
-     * @return
+     * Calcula a distancia entre dois pontos
      */
     public double distancia(Ponto2D a) {
         double dx = x - a.x;
@@ -59,8 +54,7 @@ public class Ponto2D {
     }
 
     /**
-     *
-     * @return
+     * @return Distancia entre o primeiro e o segundo ponto
      */
     @Override
     public String toString() {
